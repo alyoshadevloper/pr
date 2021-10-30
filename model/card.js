c1a594eddb6756149d56e82e9b1f0d41704a43e7
alisher <etg2018@inbox.ru>
2021-10-30 23:20:18 +0500

    asdsa

--- model/card.js
+++ model/card.js
@@ -3,31 +3,31 @@ const fs = require('fs')
 
 class Card {
 
-    static async add(course ,ips){
+    static async add(course, ips) {
         // console.log(ips);
         // console.log(course);
-        const card  = await Card.fetch()
+        const card = await Card.fetch()
         // console.log(card);
         const idx = card.courses.findIndex(c => c.id === course.id)
         const candidate = card.courses[idx]
         course.ip = ips
-        if(candidate){
+        if (candidate) {
             /// kurs korzinadan bolsa miqdorini bittaga kopaytirish
             candidate.count++
             card.courses[idx] = candidate
-        }else{
+        } else {
             /// kurs korzinadan olmasa korzinaga qoshish miqdori birga teng boladi
             course.count = 1
             card.courses.push(course)
         }
-        card.price +=  +course.price  
+        card.price += +course.price
 
         //// malumotlarni card.jsonga yozish
         return new Promise((resolve, reject) => {
-            fs.writeFile(path.join(__dirname , ".." , "data" , "card.json") , JSON.stringify(card) , err => {
-                if(err){
+            fs.writeFile(path.join(__dirname, "..", "data", "card.json"), JSON.stringify(card), err => {
+                if (err) {
                     reject(err)
-                }else{
+                } else {
                     resolve()
                 }
             })
@@ -35,22 +35,22 @@ class Card {
     }
     /// remove card in shopping bag
 
-    static async remove(id){
-        const card  = await Card.fetch()
-        const idx = card.courses.findIndex(c => c.id === id)  /// bitta mahsulotimiz
+    static async remove(id) {
+        const card = await Card.fetch()
+        const idx = card.courses.findIndex(c => c.id === id) /// bitta mahsulotimiz
         const course = card.courses[idx] // bitta mahsulot {}
-        if(course.count === 1){
+        if (course.count === 1) {
             card.courses = card.courses.filter(c => c.id !== id)
-        }else{
+        } else {
             card.courses[idx].count--
 
         }
-        card.price -=  +course.price
+        card.price -= +course.price
         return new Promise((resolve, reject) => {
-            fs.writeFile(path.join(__dirname , ".." , "data" , "card.json") , JSON.stringify(card) , err => {
-                if(err){
+            fs.writeFile(path.join(__dirname, "..", "data", "card.json"), JSON.stringify(card), err => {
+                if (err) {
                     reject(err)
-                }else{
+                } else {
                     resolve(card)
                 }
             })
@@ -59,38 +59,45 @@ class Card {
     }
 
     /// count system
-    static async counts(){
+    static async counts() {
         const card = await Card.fetch()
         let num = 0
         card.courses.forEach(element => {
             num += element.count
-            return num       
+            return num
         });
         return num
     }
 
-    static async fetch(ips ) {
+    static async fetch(ips) {
+
+        return new Promise((resolve, reject) => {
+            fs.readFile(path.join(__dirname, "..", 'data', 'card.json'), 'utf-8', async (err, data) => {
  
-        return new Promise((resolve , reject) => {
-            fs.readFile(path.join(__dirname , ".." , 'data' , 'card.json') , 'utf-8' , (err, data) => {
-                // console.log(data);
-                if(err){
+                if (err) {
                     reject(err)
-                }else{
-                         
-                            const datsa = JSON.parse(data)
-                             let arr = []
-                                datsa.courses.forEach( (element , val)   => {
-                                        if(element.ip === ips){
-                                            arr.push(element)
-                                            datsa.courses = arr
-                                        }
-                                });
-                                resolve(datsa)
-                             
-                        // // console.log(datsa);
-            
-                   
+                } else {
+                                
+                    // const datsa = await JSON.parse(data)
+                    // let arr = []
+                    // console.log( datsa.courses.filter(c =>  c.ip === ips) , 2121);
+                    // resolve(datsa)
+
+                    const datsa = await JSON.parse(data)
+                    let arr = []
+                    datsa.courses.forEach(async (element) => {
+                        if (element.ip === ips) {
+                            return await arr.push(element)
+                        }
+                    });
+                    console.log(arr);
+                    datsa.courses = await arr
+                    console.log( datsa.courses);
+                    resolve(datsa)
+
+                    // // console.log(datsa);
+
+
                 }
             })
         })
